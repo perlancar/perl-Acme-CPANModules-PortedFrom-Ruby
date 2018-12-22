6fe153e05b7e6c8cc6abe17819e96bc4e0d9cf47
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-12-22 08:30:59 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/PortedFrom/Ruby.pm
@@ -0,0 +1,26 @@
+package Acme::CPANModules::PortedFrom::Ruby;
+
+# DATE
+# VERSION
+
+our $LIST = {
+    summary => "Modules/applications that are ported from (or inspired by) ".
+        "Ruby libraries",
+    description => <<'_',
+
+If you know of others, please drop me a message.
+
+_
+    entries => [
+        {
+            module => 'App::Sass',
+            #ruby_package => undef',
+            tags => ['web'],
+        },
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
